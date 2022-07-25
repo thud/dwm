74b15f9e77edf60406f512f9a70435a43d940895
thud <thud@thud.dev>
2022-07-25 12:59:20 +0100

    Move config into `config.def.h`

--- config.def.h
+++ config.def.h
@@ -1,45 +1,50 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const int gappx				= 10;		/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating	= 0;		/* 1 means swallowing floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = {"JetBrainsMonoMedium Nerd Font:pixelsize=14",
+										"monospace:size=10",
+										"-wuncon-siji-medium-r-normal--10-100-75-75-c-80-iso10646-1" };
+static const char dmenufont[]          = "JetBrainsMonoMedium:pixelsize=14\",\"-wuncon-siji-medium-r-normal--10-100-75-75-c-80-iso10646-1";
+static const char col_gray1[]       = "#2E3440";
+static const char col_gray2[]       = "#3B4252";
+static const char col_gray3[]       = "#D8DEE9";
+static const char col_gray4[]       = "#ECEFF4";
+static const char col_cyan[]        = "#5E81AC";
+static const char col_red[]			= "#BF616A";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_red   },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class	  		instance    title       	tags mask	isfloating  isterminal  noswallow	monitor */
+	{ "Gimp",	  		NULL,		NULL,			0,			1,			0,			0,			-1 },
+	{ "brave-browser",	NULL,	  	NULL,			1 << 8,		0,			0,			-1,			-1 },
+	{ "Alacritty",		NULL,		NULL,			0,			0,			1,			0,			-1 },
+	{ "mpv",			NULL,		NULL,			0,			0,			0,			0,			-1 },
+	{ NULL,		  		NULL,		"Python Turtle Graphics",	0,			0,			0,			1,			-1 },
+	{ NULL,		  		NULL,		"Event Tester", 0,			0,			0,			1,			-1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[]=",     tile },    /* first entry is default */
+//	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -56,14 +61,19 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, topbar ? NULL : "-b", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termxcmd[]  = { "alacritty", "-e", "tmux", NULL };
 
+#include <X11/XF86keysym.h>
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termxcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -84,6 +94,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -94,6 +107,20 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+	{ MODKEY,						XK_e,	   				   spawn,		   SHCMD("setbg") },
+	{ MODKEY,						XK_r,	   				   spawn,		   SHCMD("screentoggle") },
+	{ ControlMask,					XK_space,  				   spawn,		   SHCMD("dunstctl close") },
+	{ ControlMask|ShiftMask,		XK_space,  				   spawn,		   SHCMD("dunstctl close-all") },
+ 	{ MODKEY,						XK_a,	   				   spawn,		   SHCMD("$BROWSER") },
+	{ MODKEY|ShiftMask,				XK_a,	   				   spawn,		   SHCMD("$BROWSER --private-window --incognito") },
+	{ 0,							XF86XK_MonBrightnessDown,  spawn,		   SHCMD("xbacklight -ctrl intel_backlight -10") },
+	{ 0,							XF86XK_MonBrightnessUp,	   spawn,		   SHCMD("xbacklight -ctrl intel_backlight +10") },
+	{ Mod1Mask,						XF86XK_MonBrightnessDown,  spawn,		   SHCMD("xbacklight -ctrl tpacpi::kbd_backlight -10") },
+	{ Mod1Mask,						XF86XK_MonBrightnessUp,	   spawn,		   SHCMD("xbacklight -ctrl tpacpi::kbd_backlight +10") },
+    { 0,							XF86XK_AudioMute,   	   spawn,		   SHCMD("volume m") },
+    { 0,							XF86XK_AudioRaiseVolume,   spawn,		   SHCMD("volume i") },
+    { 0,							XF86XK_AudioLowerVolume,   spawn,		   SHCMD("volume d") },
 };
 
 /* button definitions */
